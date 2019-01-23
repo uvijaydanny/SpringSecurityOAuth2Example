e40447394f187a85ae1e039462d3d34a2578dba2
Danny <danny.undrajana@senecaglobal.net>
2019-01-23 15:20:56 +0530

    oAuth2 Example

---
+++ src/main/java/com/danny/springmvc/configuration/HelloWorldConfiguration.java
@@ -0,0 +1,13 @@
+package com.danny.springmvc.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.danny.springmvc")
+public class HelloWorldConfiguration {
+	
+
+}
